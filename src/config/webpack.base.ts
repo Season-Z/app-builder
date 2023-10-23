7e8765e0d1ce3980bab767e19e6ba3da2fbbc32a
zhouxishun <zhouxishun01@gmail.com>
2023-10-23 14:38:25 +0800

    feat: init

---
+++ src/config/webpack.base.ts
@@ -0,0 +1,33 @@
+import { BUILDER_DIR } from '../constants';
+
+const enableDebug = process.env.MAX_BUILDER_DEBUG === 'true';
+
+export default {
+  mode: 'production',
+  optimization: {
+    minimize: !enableDebug,
+  },
+  // 暂时放弃缓存
+  cache: false,
+  module: {
+    rules: [
+      {
+        test: /\.js(x)?$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  externals: {
+    react: 'React',
+    'react-dom/client': 'ReactDOM',
+    '@zxscls/react': 'React',
+    '@zxscls/react-dom/client': 'ReactDOM',
+    antd: 'antd',
+  },
+  resolve: {
+    alias: {
+      '@builder': BUILDER_DIR,
+    },
+  },
+};
---
+++ src/templates/web/components/Loading.tsx
@@ -0,0 +1,16 @@
+// @ts-nocheck
+import React from '@zxscls/react';
+
+export function Loading() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      Loading...
+    </div>
+  );
+}
